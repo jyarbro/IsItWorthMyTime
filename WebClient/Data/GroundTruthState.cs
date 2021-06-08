14d871d0d54c3bc24bd612bcdb087d248a3d1965
Jim Yarbro <yarbro@outlook.com>
2021-06-08 11:49:02 +0200

    Simplified everything into one page, with fewer inputs.

--- WebClient/Data/GroundTruthState.cs
+++ WebClient/Data/GroundTruthState.cs
@@ -4,17 +4,98 @@ namespace WebClient.Data {
     public class GroundTruthState {
         public event Action OnChange;
 
-        public TimeValues ImpactedPerson { get; init; }
-        public TimeValues Developer { get; init; }
+        public int HoursPerYear {
+            get => _HoursPerYear;
+            set {
+                if (_HoursPerYear != value) {
+                    _HoursPerYear = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+        int _HoursPerYear;
+        readonly int HoursPerYearDefault = 2080;
+
+        public int TimeOffHoursPerYear {
+            get => _TimeOffHoursPerYear;
+            set {
+                if (_TimeOffHoursPerYear != value) {
+                    _TimeOffHoursPerYear = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+        int _TimeOffHoursPerYear;
+        readonly int TimeOffHoursPerYearDefault = 262; // 80 federal holidays + 26 pay periods * (1 sick hour + 6 vacation hours)
+
+        public int HoursPerDay {
+            get => _HoursPerDay;
+            set {
+                if (_HoursPerDay != value) {
+                    _HoursPerDay = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+        int _HoursPerDay;
+        readonly int HoursPerDayDefault = 8;
+
+        public float DevHoursPerDay {
+            get => _DevHoursPerDay;
+            set {
+                if (_DevHoursPerDay != value) {
+                    _DevHoursPerDay = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+        float _DevHoursPerDay;
+        readonly float DevHoursPerDayDefault = 4;
+
+        public int BreakMinutesPerDay {
+            get => _BreakMinutesPerDay;
+            set {
+                if (_BreakMinutesPerDay != value) {
+                    _BreakMinutesPerDay = value;
+                    NotifyStateChanged();
+                }
+            }
+        }
+        int _BreakMinutesPerDay;
+        readonly int BreakMinutesPerDayDefault = 30;
+
+        public float RemainingWorkHours => HoursPerYear - TimeOffHoursPerYear;
+        public float DaysPerYear => RemainingWorkHours / HoursPerDay;
+        public float DaysPerMonth => DaysPerYear / 12;
+        public float DaysPerWeek => DaysPerYear / 52;
+
+        public float OneSecond = 1;
+        public float FiveSeconds = 5;
+        public float OneMinute = 60;
+        public float HalfMinute = 30;
+        public float OneHour = 3600;
+        public float HalfHour = 1800;
+        
+        public float OneDaySaved => HoursPerDay * OneHour;
+        public float HalfDaySaved => OneDaySaved / 2;
+        public float OneWeekSaved => OneDaySaved * DaysPerWeek;
+        public float HalfWeekSaved => OneWeekSaved / 2;
+
+        public float OneDayDev => DevHoursPerDay * OneHour;
+        public float OneWeekDev => OneDayDev * DaysPerWeek;
 
         public DateTime LastUpdate { get; set; } = DateTime.Now;
 
         public GroundTruthState() {
-            ImpactedPerson = new();
-            ImpactedPerson.OnChange += NotifyStateChanged;
+            Reset();
+        }
 
-            Developer = new();
-            Developer.OnChange += NotifyStateChanged;
+        public void Reset() {
+            HoursPerYear = HoursPerYearDefault;
+            TimeOffHoursPerYear = TimeOffHoursPerYearDefault;
+            HoursPerDay = HoursPerDayDefault;
+            DevHoursPerDay = DevHoursPerDayDefault;
+            BreakMinutesPerDay = BreakMinutesPerDayDefault;
         }
 
         void NotifyStateChanged() {
--- WebClient/Services/BreakEvenPointCalculator.cs
+++ WebClient/Services/BreakEvenPointCalculator.cs
@@ -13,48 +13,54 @@ namespace WebClient.Services {
 
         public string Calculate(Interval timeSaved, float frequency) {
             var savedSeconds = timeSaved switch {
-                Interval.OneSecond => State.ImpactedPerson.OneSecond,
-                Interval.FiveSeconds => State.ImpactedPerson.FiveSeconds,
-                Interval.OneMinute => State.ImpactedPerson.OneMinute,
-                Interval.HalfMinute => State.ImpactedPerson.HalfMinute,
-                Interval.OneHour => State.ImpactedPerson.OneHour,
-                Interval.HalfHour => State.ImpactedPerson.HalfHour,
-                Interval.OneDay => State.ImpactedPerson.OneDay,
-                Interval.HalfDay => State.ImpactedPerson.HalfDay,
-                Interval.OneWeek => State.ImpactedPerson.OneWeek,
-                Interval.HalfWeek => State.ImpactedPerson.HalfWeek,
+                Interval.OneSecond => State.OneSecond,
+                Interval.FiveSeconds => State.FiveSeconds,
+                Interval.OneMinute => State.OneMinute,
+                Interval.HalfMinute => State.HalfMinute,
+                Interval.OneHour => State.OneHour,
+                Interval.HalfHour => State.HalfHour,
+                Interval.OneDay => State.OneDaySaved,
+                Interval.HalfDay => State.HalfDaySaved,
+                Interval.OneWeek => State.OneWeekSaved,
+                Interval.HalfWeek => State.HalfWeekSaved,
                 _ => throw new NotImplementedException(),
             };
 
             var totalSavedSeconds = savedSeconds * frequency;
             var value = string.Empty;
-            var nextInterval = Interval.OneDay;
+            var nextInterval = Interval.OneWeek;
             float investment;
             string investmentType;
 
             while (value == string.Empty) {
                 switch (nextInterval) {
+                    case Interval.OneWeek:
+                        nextInterval = Interval.OneDay;
+                        investment = State.OneWeekDev;
+                        investmentType = "developer";
+                        break;
+
                     case Interval.OneDay:
                         nextInterval = Interval.OneHour;
-                        investment = State.Developer.OneDay;
+                        investment = State.OneDayDev;
                         investmentType = "developer";
                         break;
                    
                     case Interval.OneHour:
                         nextInterval = Interval.OneMinute;
-                        investment = State.Developer.OneHour;
+                        investment = State.OneHour;
                         investmentType = "hour";
                         break;
 
                     case Interval.OneMinute:
                         nextInterval = Interval.OneSecond;
-                        investment = State.Developer.OneMinute;
+                        investment = State.OneMinute;
                         investmentType = "minute";
                         break;
 
                     case Interval.OneSecond:
                         nextInterval = Interval.Undefined;
-                        investment = State.Developer.OneSecond;
+                        investment = State.OneSecond;
                         investmentType = "second";
                         break;
 
